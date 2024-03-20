25a78ea58fdbd339d0aa252d3ec5ba1ca01050d8
bagasdhitya98 <bagasdhityataufiqqi21@gmail.com>
2024-03-20 12:52:33 +0700

    add: create card and landing components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 
-const Card = ({ children, onClick, props }) => {
+const Card = ({ children, onClick, props, type }) => {
   return (
-    <div
-      onClick={onClick}
-      {...props}
-      className="w-full h-full flex flex-column justify-center items-center gap-x-5 p-5 rounded-md shadow-md cursor-pointer bg-white hover:bg-slate-100"
-    >
-      {children}
-    </div>
+    <>
+      {type === "list" ? (
+        <div
+          onClick={onClick}
+          {...props}
+          className="w-full h-full flex flex-column justify-center items-center gap-x-5 p-5 rounded-md shadow-md cursor-pointer bg-white hover:bg-slate-100"
+        >
+          {children}
+        </div>
+      ) : (
+        type === "information" && (
+          <div
+            {...props}
+            className="w-full h-full lg:flex md:flex justify-center items-center gap-x-5 p-5 rounded-md shadow-md bg-white"
+          >
+            {children}
+          </div>
+        )
+      )}
+    </>
   );
 };
 
