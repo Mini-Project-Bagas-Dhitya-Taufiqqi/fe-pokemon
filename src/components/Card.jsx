2ecb207eac956f1c77d018d6814e1b77c96868ac
bagasdhitya98 <bagasdhityataufiqqi21@gmail.com>
2024-03-20 10:43:02 +0700

    add: create component for card props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-const Card = ({ children }) => {
+const Card = ({ children, onClick, props }) => {
   return (
-    <div className="w-full h-full flex flex-column justify-center items-center gap-x-5 p-5 rounded-md shadow-md">
+    <div
+      onClick={onClick}
+      {...props}
+      className="w-full h-full flex flex-column justify-center items-center gap-x-5 p-5 rounded-md shadow-md cursor-pointer hover:bg-slate-100"
+    >
       {children}
     </div>
   );
