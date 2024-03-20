8280f23356167400ab2f7e634f3c65eaaa3269a3
bagasdhitya98 <bagasdhityataufiqqi21@gmail.com>
2024-03-20 11:47:39 +0700

    add: added transition on each page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
 import PokemonListPage from "../pages/pokemon-list-page";
 import PokemonDetail from "../pages/pokemon-detail";
@@ -7,12 +8,48 @@ import MyPokemon from "../pages/my-pokemon";
 const PokemonRouter = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PokemonListPage />} />
-        <Route path="/pokemon-detail" element={<PokemonDetail />} />
-        <Route path="/my-pokemon" element={<MyPokemon />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <FadeIn>
+                <PokemonListPage />
+              </FadeIn>
+            }
+          />
+          <Route
+            path="/pokemon-detail"
+            element={
+              <FadeIn>
+                <PokemonDetail />
+              </FadeIn>
+            }
+          />
+          <Route
+            path="/my-pokemon"
+            element={
+              <FadeIn>
+                <MyPokemon />
+              </FadeIn>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
     </BrowserRouter>
+  );
+};
+
+const FadeIn = ({ children }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      {children}
+    </motion.div>
   );
 };
 
